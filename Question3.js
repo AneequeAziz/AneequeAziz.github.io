c68d2314496a5386ec3b05120a068edbb3d7c780
AneequeAziz <aneequeaziz123@gmail.com>
2021-07-29 19:48:06 +0500

    New

--- Question3.js
+++
@@ -1,8 +0,0 @@
-function Mobile (company,model,price,color){
-    this.company = company;
-    this.model = model;
-    this.price = price;
-    this.color = color;
-}
-var myMobile = new Mobile("Samsung","S6",28000,"Golden");
-alert("My Mobile Price is: " +myMobile.price+'.');
